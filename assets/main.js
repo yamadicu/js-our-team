63d49bad2b52c23b239869fe6e831987940b453b
yamadicu <dicutia@gmail.com>
2023-04-12 15:27:09 +0200

    fine milestone 1

---
+++ assets/main.js
@@ -0,0 +1,38 @@
+const team = [
+    {
+      nome: 'Wayne Barnett',
+      ruolo: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      nome: 'Angela Caroll',
+      ruolo: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      nome: 'Walter Gordon',
+      ruolo: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      nome: 'Angela Lopez',
+      ruolo: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      nome: 'Scott Estrada',
+      ruolo: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      nome: 'Barbara Ramos',
+      ruolo: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+  for ( let i = 0; i < team.length; i++ ){
+
+    console.log(team[i]);
+
+  }
